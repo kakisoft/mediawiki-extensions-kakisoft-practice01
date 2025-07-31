48ef61b3384abe51ee6dfbf878228293d80660f6
kakisoft <kakisttab@gmail.com>
2025-07-31 09:42:33 +0900

    edit Main

--- KakisoftPractice01Hooks.php
+++ KakisoftPractice01Hooks.php
@@ -1,12 +1,19 @@
 <?php
 
 class KakisoftPractice01Hooks {
+    // フックで必要な構文登録処理
+    public static function onParserFirstCallInit( Parser $parser ) {
+        $parser->setFunctionHook( 'hello', [ self::class, 'renderHello' ] );
+        return true;
+    }
+
+    // パーサ関数の本体
     public static function renderHello( Parser $parser ) {
         return [ self::buildOutput(), 'noparse' => true ];
     }
 
+    // 表示内容を返すメイン処理
     private static function buildOutput(): string {
         return 'Hello World';
     }
-
 }
